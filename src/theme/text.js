4bb918d46601c109c0a122ed69a798a127da6bf4
DESKTOP-M3G54LB\catal <raczcatalin22@yahoo.com>
2021-06-23 18:36:16 +0300

    Revert "Revert "challenge""
    This reverts commit 9857dc9a67f8199df3b07d4c0905245a1d660a92.

---
+++ src/theme/text.js
@@ -0,0 +1,8 @@
+export const strings = {
+  streetAngars: "Street food Angars",
+  streetCircus: "Street food Circus",
+  streetIsraeli: "Street food Israeli",
+  ore6: "acum 6 ore",
+  ore5: "acum 5 ore",
+  ore4: "acum 4 ore"
+}
