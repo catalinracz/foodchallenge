9857dc9a67f8199df3b07d4c0905245a1d660a92
DESKTOP-M3G54LB\catal <raczcatalin22@yahoo.com>
2021-06-23 18:35:45 +0300

    Revert "challenge"
    This reverts commit c79479c5b6a2ab5d182db7bbfba434142bb25d8a.

--- src/theme/images.js
+++
@@ -1,5 +0,0 @@
-export const Images = {
-  angars: require('../../assets/angars.jpg'),
-  circus: require('../../assets/circus.jpg'),
-  israeli: require('../../assets/israeli.jpg'),
-};
--- src/theme/text.js
+++
@@ -1,8 +0,0 @@
-export const strings = {
-  streetAngars: "Street food Angars",
-  streetCircus: "Street food Circus",
-  streetIsraeli: "Street food Israeli",
-  ore6: "acum 6 ore",
-  ore5: "acum 5 ore",
-  ore4: "acum 4 ore"
-}
